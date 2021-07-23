cbd70abcf6368ba1d982808ff7389d86fca47e51
Will Tackett <will.s.tackett@gmail.com>
2021-07-23 16:32:12 -0400

    add sex to regression

--- code/scripts/calc_coeffs.py
+++ code/scripts/calc_coeffs.py
@@ -11,6 +11,9 @@ import pandas as pd
 import numpy as np
 import os
 import glob
+import math
+import datetime
+from sklearn.linear_model import LinearRegression
 
 os.chdir('/media/will/My Passport/Ubuntu/cortical_thickness_maps/stats')
 files = glob.glob('*schaefer.csv')
@@ -36,7 +39,6 @@ atlas=pd.read_csv('/home/will/Projects/healthy-t1-dataset/labels/Schaefer2018_20
 wblabs=atlas["label_number"]
 
 #define function needed to calculate standard error of regressors
-import math
 def RSE(y_true, y_predicted):
     y_true = np.array(y_true)
     y_predicted=np.array(y_predicted)
@@ -46,33 +48,43 @@ def RSE(y_true, y_predicted):
     return rse
 
 #loop through each label and plug data into linear model
-from sklearn.linear_model import LinearRegression
 
 int_coff=[]
-target_coff=[]
+target_age_coff=[]
+target_sex_coff=[]
 rse=[]
 ilabs=[]
 for ind in range(0,len(wblabs)):
-  i=wblabs[ind]
-  tmplabs=temp_vals[temp_vals['label_number'] == i]
-  df=pd.DataFrame(tmplabs.value)
-  df=pd.concat([df.reset_index(drop=True), age_vals], axis=1)
-  X= df['Age'].values.reshape(-1,1)
-  Y= df['value'].values.reshape(-1,1)
-  linear_regressor = LinearRegression()  # create object for the class
-  linear_regressor.fit(X, Y)  # perform linear regression
-  Y_pred=linear_regressor.predict(X)  # make predictions
-  int_coff.append(linear_regressor.intercept_[0])
-  target_coff.append(linear_regressor.coef_[0][0])
-  rse.append(RSE(Y,Y_pred))
-  ilabs.append(i)
+    i=wblabs[ind]
+    tmplabs=temp_vals[temp_vals['label_number'] == i]
+    df=pd.DataFrame(tmplabs.value)
+    df=pd.concat([df.reset_index(drop=True), age_vals], axis=1)
+    #X= df['Age'].values.reshape(-1,1)
+    X=df[['Age','SexNumeric']].values.reshape(-1,2)
+    Y=df['value'].values.reshape(-1,1)
+    linear_regressor = LinearRegression()  # create object for the class
+    linear_regressor.fit(X, Y)  # perform linear regression
+    Y_pred=linear_regressor.predict(X)  # make predictions
+    int_coff.append(linear_regressor.intercept_[0])
+    target_age_coff.append(linear_regressor.coef_[0][0])
+    target_sex_coff.append(linear_regressor.coef_[0][1])
+    rse.append(RSE(Y,Y_pred))
+    ilabs.append(i)
 
 wsCoffs = pd.DataFrame.from_dict({
     'label_number':ilabs,
     'intercept':int_coff,
-    'age_coefficient':target_coff,
+    'age_coefficient':target_age_coff,
+    'sex_coefficient':target_sex_coff,
     'residual_se':rse,
     })
 
 
-wsCoffs.to_csv('/home/will/Projects/healthy-t1-dataset/ws_coeffs.csv', index=False)
+
+#current date and time
+now = datetime.datetime.now()
+dformat = "%m-%d-%Y"
+date = now.strftime(dformat)
+
+
+wsCoffs.to_csv('/home/will/Projects/healthy-t1-dataset/ws_coeffs_{}.csv'.format(date), index=False)
